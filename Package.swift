3948112a84e94b78b6dc567f513c3ec48cf1b7be
Abedalkareem <abedalkareem.omreyh@yahoo.com>
2019-12-15 14:52:54 +0400

    change header text

--- Package.swift
+++ Package.swift
@@ -1,8 +1,12 @@
-// swift-tools-version:5.0
 //
-//  Package.swift
+//  LanguageManager.swift
 //
-//  Copyright (c) 2014-2018 Alamofire Software Foundation (http://alamofire.org/)
+//  Created by abedalkareem omreyh on 10/23/17.
+//  Copyright © 2017 abedlkareem omreyh. All rights reserved.
+//  GitHub: https://github.com/Abedalkareem/LanguageManager-iOS
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2017 Abedalkareem
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -11,16 +15,16 @@
 //  copies of the Software, and to permit persons to whom the Software is
 //  furnished to do so, subject to the following conditions:
 //
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
 //
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 //  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 //  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 //  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 //  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 //
 
 import PackageDescription
