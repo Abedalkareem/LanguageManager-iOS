f4d64d53471987ba84f21fe5415ec3a04b73706c
Abedalkareem <abedalkareem.omreyh@yahoo.com>
2020-11-10 13:49:07 +0400

    create storage class

---
+++ LanguageManager-iOS/Classes/Constants/DefaultsKeys.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultsKeys.swift
+//  LanguageManager-iOS
+//
+//  Created by abedalkareem omreyh on 10/11/2020.
+//
+
+import Foundation
+
+enum DefaultsKeys: String {
+  case selectedLanguage = "LanguageManagerSelectedLanguage"
+  case defaultLanguage = "LanguageManagerDefaultLanguage"
+}
